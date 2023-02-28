afc772c23a6da45f586ecff55ce322aea19598cd
deedeemat <121471595+deedeemat@users.noreply.github.com>
2023-02-27 20:44:35 -0500

    Update car.py

--- car.py
+++ car.py
@@ -1,9 +1,13 @@
+import unittests
+import function tests
 from serviceable import Serviceable
 
 
 class Car(Serviceable):
     def __init__(self, engine, battery):
+        def test_add(self):
         self.engine = engine
+        self.assertEqual(functions.add(engine, battery), true)
         self.battery = battery
 
     def needs_service(self):
